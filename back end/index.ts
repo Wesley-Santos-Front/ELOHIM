71a63d29474cb6284e93195bbd478506825d232c
Wesley-Santos-Front <wesleytksantos321@gmail.com>
2026-09-24 00:21:25 -0300

    ola

--- back end/index.ts
+++ back end/index.ts
@@ -13,7 +13,18 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+        // Permite requisições sem Origin, como algumas ferramentas/API clients
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        return callback(new Error("Origem não permitida pelo CORS"));
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
